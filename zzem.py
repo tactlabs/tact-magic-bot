466b52691ca4c4542e32a32261ac9d8266871e0a
Soundarya Murugan <soundaryamurugan2002@gmail.com>
2025-02-05 20:34:03 +0530

    changes in basic requirements

---
+++ zzem.py
@@ -0,0 +1,17 @@
+
+# pylint: disable=missing-function-docstring, pointless-string-statement, missing-class-docstring
+
+'''
+@author: Soundarya
+
+source:
+    ?
+'''
+
+def startpy():
+
+    print("Tact101")
+
+
+if __name__ == '__main__':
+    startpy()
